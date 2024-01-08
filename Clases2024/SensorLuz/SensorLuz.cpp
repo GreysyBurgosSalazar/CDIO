7e3fe175cebf5bfeb6a6751fe58bb6c4587f5ff3
RebecaMAy <144907582+RebecaMAy@users.noreply.github.com>
2024-01-08 20:02:42 +0100

    Clases 2024

---
+++ Clases2024/SensorLuz/SensorLuz.cpp
@@ -0,0 +1,33 @@
+#include "SensorLuz.h"
+#include <Arduino.h>
+
+
+SensorLuz::SensorLuz(int canal, Adafruit_ADS1115& ads) : canalADS(canal), ads1(ads){
+}
+
+float SensorLuz::lecturaLuz() {
+  float valorDigital = ads1.readADC_SingleEnded(canalADS);
+  float luz = valorDigital * 4.096 / 32767;
+  return luz;
+  /*if (luz > 0.04) {
+    return 10; // luz artificial
+  } else if (luz > 0) {
+    return 1; // luz natural
+  } else {
+    return 0; // oscuro
+  }*/
+}
+
+void SensorLuz::mostrarLuz() {
+  float lectura = lecturaLuz();
+  Serial.println("Sistema de iluminación: ");
+  Serial.println(lectura);
+  /*if (lectura < 0) {
+    Serial.println("Apagado. ····· A oscuras.");
+  } else if (lectura > 0) {
+    Serial.println("Activado.");*/
+  /*} else {
+    Serial.println("Apagado. ····· Luz natural.");
+  }*/
+//}
+}
